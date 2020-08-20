5c72f7ce19fd56f86a3386d5f76ddfbe0f923cef
kalend.zhang <A@1234cn>
2020-08-20 19:44:57 +0800

    update 1.0.7 1. CacheService  修改清除本地缓存时没有开监控情况下导致清除报错的问题

--- layering-cache-tool/src/main/java/com/github/zomin/tool/service/CacheService.java
+++ layering-cache-tool/src/main/java/com/github/zomin/tool/service/CacheService.java
@@ -41,18 +41,20 @@ public class CacheService {
             for (AbstractCacheManager cacheManager : cacheManagers) {
                 // 删除缓存统计信息
                 String redisKey = StatsService.CACHE_STATS_KEY_PREFIX + cacheName + internalKey;
-                BeanFactory.getBean(StatsService.class).resetCacheStat(redisKey);
-
+                if(cacheManager.getStats()) {
+                    BeanFactory.getBean(StatsService.class).resetCacheStat(redisKey);
+                }
                 // 删除缓存
                 Collection<Cache> caches = cacheManager.getCache(cacheName);
                 if (CollectionUtils.isEmpty(caches)) {
                     // 如果没有找到Cache就新建一个默认的
                     Cache cache = cacheManager.getCache(cacheName, defaultSetting);
                     cache.clear();
-
-                    // 删除统计信息
-                    redisKey = StatsService.CACHE_STATS_KEY_PREFIX + cacheName + defaultSetting.getInternalKey();
-                    BeanFactory.getBean(StatsService.class).resetCacheStat(redisKey);
+                    if(cacheManager.getStats()) {
+                        // 删除统计信息
+                        redisKey = StatsService.CACHE_STATS_KEY_PREFIX + cacheName + defaultSetting.getInternalKey();
+                        BeanFactory.getBean(StatsService.class).resetCacheStat(redisKey);
+                    }
                 } else {
                     for (Cache cache : caches) {
                         cache.clear();
